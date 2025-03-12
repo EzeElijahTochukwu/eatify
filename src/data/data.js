4814090ccec0e73f74cfeb5d4289445331e4a25a
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-03-12 23:57:40 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export const data = [
   {
     id: 1,
-    name: 'Ceasar Salad',
+    name: 'Salad',
     category: 'salad',
     image:
       'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
@@ -25,7 +25,7 @@ export const data = [
   },
   {
     id: 4,
-    name: ' Burger',
+    name: ' Burger King',
     category: 'burger',
     image:
       'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
@@ -33,7 +33,7 @@ export const data = [
   },
   {
     id: 5,
-    name: 'Wings',
+    name: 'Chicken Wings',
     category: 'chicken',
     image:
       'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
